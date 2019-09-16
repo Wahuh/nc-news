4a11075c8d0c834d30591c63ec44ca8fe92d83ee
Thanh <tmdoan98@gmail.com>
2019-09-16 13:53:19 +0100

    add App routing and layout

--- src/app/components/App/index.js
+++ src/app/components/App/index.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { Router } from "@reach/router";
+import { Flex } from "rebass";
 import "./App.css";
+import Header from "../Header";
+import Home from "../../../home/components/Home";
+import TopicsDropdown from "../../../topics/components/TopicsDropdown";
+import Topic from "../../../topics/components/Topic";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Flex sx={{ height: "100vh", width: "100%" }} flexDirection="column">
+      <Header>
+        <TopicsDropdown />
+      </Header>
+
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        sx={{ flex: 1 }}
+        as="main"
+      >
+        <Router>
+          <Home path="/" />
+          <Topic path="/t/:topic" />
+        </Router>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default App;
