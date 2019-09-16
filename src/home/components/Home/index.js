86d601298defb2192c1065a1be515019d47f71c1
Thanh <tmdoan98@gmail.com>
2019-09-16 21:41:03 +0100

    refactor to functional components and move data fetching into ArticlesList

--- src/home/components/Home/index.js
+++ src/home/components/Home/index.js
@@ -1,21 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ArticlesList from "../../../articles/components/ArticlesList";
-import api from "../../../api";
 
-class Home extends Component {
-  state = {
-    articles: []
-  };
-
-  async componentDidMount() {
-    const articles = await api.getArticles();
-    this.setState({ articles });
-  }
-
-  render() {
-    const { articles } = this.state;
-    return <ArticlesList articles={articles} />;
-  }
-}
+const Home = () => {
+  return <ArticlesList />;
+};
 
 export default Home;
--- src/topics/components/Topic/index.js
+++ src/topics/components/Topic/index.js
@@ -1,39 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import ArticlesList from "../../../articles/components/ArticlesList";
-import api from "../../../api";
 
-class Topic extends Component {
-  state = {
-    articles: []
-  };
-
-  async componentDidMount() {
-    const { topic } = this.props;
-    await this.fetchArticles(topic);
-  }
-
-  async componentDidUpdate(prevProps) {
-    const { topic } = this.props;
-    if (prevProps.topic !== topic) {
-      await this.fetchArticles(topic);
-    }
-  }
-
-  async fetchArticles(topic) {
-    const articles = await api.getArticles(topic);
-    this.setState({ articles });
-  }
-
-  render() {
-    const { articles } = this.state;
-    const { children } = this.props;
-    return (
-      <>
-        {children}
-        <ArticlesList articles={articles} />
-      </>
-    );
-  }
-}
+const Topic = ({ children, topic }) => {
+  return (
+    <>
+      {children}
+      <ArticlesList topic={topic} />
+    </>
+  );
+};
 
 export default Topic;
