a49e4bb6cfea49b92443ac209004b204655eb4ea
Thanh <tmdoan98@gmail.com>
2019-09-17 23:10:46 +0100

    refactor ArticleVotes design

--- src/articles/ArticleVotes.js
+++ src/articles/ArticleVotes.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { Button, Flex } from "rebass";
+import UpIcon from "../common/UpIcon";
+import DownIcon from "../common/DownIcon";
 
 const ArticleVotes = ({ votes, onUpvote, onDownvote }) => {
   return (
-    <Flex flexDirection="column">
-      <Button onClick={onUpvote}>Upvote</Button>
+    <Flex flexDirection="column" alignItems="center" justifyContent="center">
+      <Button
+        sx={{
+          border: "none",
+          backgroundColor: "transparent",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          svg: {
+            height: "20px",
+            width: "20px",
+            fill: "black"
+          }
+        }}
+        onClick={onUpvote}
+      >
+        <UpIcon />
+      </Button>
       {votes}
-      <Button onClick={onDownvote}>Downvote</Button>
+      <Button
+        sx={{
+          border: "none",
+          backgroundColor: "transparent",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          svg: {
+            height: "20px",
+            width: "20px",
+            fill: "black"
+          }
+        }}
+        onClick={onDownvote}
+      >
+        <DownIcon />
+      </Button>
     </Flex>
   );
 };
