9295f8368d7f27aa49c73e28bda8c3b5e541575a
Thanh <tmdoan98@gmail.com>
2019-09-17 21:56:16 +0100

    add Spinner component

---
+++ src/common/Spinner.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box } from "rebass";
+import styles from "./Spinner.module.css";
+import StarIcon from "./StarIcon";
+
+const Spinner = () => {
+  return (
+    <div className={styles.Spinner}>
+      <div className={styles.ring}>
+        <div />
+        <div />
+        <div />
+      </div>
+      
+      <div className={styles.Star}>
+        <div className={styles.DoubleBounce1}>
+          <StarIcon />
+        </div>
+        <div className={styles.DoubleBounce2}>
+          <StarIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
