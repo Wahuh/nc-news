df8adb43eb82e001330b5d3a5b8218b5370dda4d
Thanh <tmdoan98@gmail.com>
2019-09-18 22:50:30 +0100

    add responsive styles

--- src/errors/ErrorPage.js
+++ src/errors/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Text } from "rebass";
+import { Box, Flex, Text } from "rebass";
 import ActionLink from "../common/ActionLink";
 import Error404 from "./Error404";
 import Error500 from "./Error500";
@@ -12,19 +12,27 @@ const errorComponents = {
 const ErrorPage = ({ code, message }) => {
   return (
     <Flex
-      flexDirection="column"
+      flexDirection={{ default: "column", lg: "row" }}
       justifyContent="center"
       alignItems="center"
       sx={{ height: "100%" }}
     >
-      <Text fontSize={16} lineHeight={1.1} fontWeight={600}>
-        {code || 404}
-      </Text>
-      <Text color="body" mb={4} fontSize={3} letterSpacing={1} fontWeight={600}>
-        {message || "PAGE NOT FOUND"}
-      </Text>
-      <Flex mb={7}>
-        <ActionLink to="/">GO HOME</ActionLink>
+      <Flex flexDirection="column" alignItems="center" mr={{ lg: 9 }}>
+        <Text fontSize={16} lineHeight={1.1} fontWeight={600}>
+          {code || 404}
+        </Text>
+        <Text
+          color="body"
+          mb={4}
+          fontSize={3}
+          letterSpacing={1}
+          fontWeight={600}
+        >
+          {message || "PAGE NOT FOUND"}
+        </Text>
+        <Flex mb={{ default: 7, lg: 0 }}>
+          <ActionLink to="/">GO HOME</ActionLink>
+        </Flex>
       </Flex>
       {code ? errorComponents[code] : errorComponents[500]}
     </Flex>
