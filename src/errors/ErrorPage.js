f47d5e68e891e578fa52c64f70c0fe72c26b875c
Thanh <tmdoan98@gmail.com>
2019-09-18 22:11:23 +0100

    refactor 404 page design

--- src/errors/ErrorPage.js
+++ src/errors/ErrorPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "@reach/router";
 import { Flex, Text } from "rebass";
 import { Cat } from "react-kawaii";
+import ActionLink from "../common/ActionLink";
 
 const ErrorPage = () => {
   return (
@@ -11,10 +12,16 @@ const ErrorPage = () => {
       alignItems="center"
       sx={{ height: "100%" }}
     >
-      <Text>404</Text>
-      <Text>PAGE NOT FOUND</Text>
-      <Cat mood="sad" />
-      <Link to="/">GO HOME</Link>
+      <Text fontSize={16} lineHeight={1.1} fontWeight={600}>
+        404
+      </Text>
+      <Text color="body" mb={4} fontSize={3} letterSpacing={1} fontWeight={600}>
+        PAGE NOT FOUND
+      </Text>
+      <Flex mb={7}>
+        <ActionLink to="/">GO HOME</ActionLink>
+      </Flex>
+      <Cat size={200} mood="sad" color="hotpink" />
     </Flex>
   );
 };
