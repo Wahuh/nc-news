c154ea2e2e95ee4a52b2fe5fd62103ad677bbf1b
Thanh <tmdoan98@gmail.com>
2019-09-20 08:33:37 +0100

    add resuable components

---
+++ src/common/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex } from "rebass";
+import styles from "./Modal.module.css";
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <Flex
+      width="100%"
+      height="100%"
+      sx={{ position: "absolute", top: 0, left: 0, zIndex: 1 }}
+    >
+      <div onClick={onClose} className={styles.ModalOverlay}>
+        {children}
+      </div>
+    </Flex>
+  );
+};
+
+export default Modal;
--- src/common/Spinner.js
+++ src/common/Spinner.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { Box } from "rebass";
-import styles from "./Spinner.module.css";
-import StarIcon from "./StarIcon";
+import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className={styles.SpinnerContainer}>
-      <div className={styles.Spinner}>
-        <div className={styles.ring}>
-          <div />
-          <div />
-          <div />
-        </div>
-
-        <div className={styles.Star}>
-          <div className={styles.DoubleBounce1}>
-            <StarIcon />
-          </div>
-          <div className={styles.DoubleBounce2}>
-            <StarIcon />
-          </div>
-        </div>
-      </div>
+    <div className="spinner">
+      <div className="dot1"></div>
+      <div className="dot2"></div>
     </div>
   );
 };
