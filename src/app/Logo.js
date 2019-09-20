1a9b9fe1e02f7501a6464462a06118047888078d
Thanh <tmdoan98@gmail.com>
2019-09-20 09:51:55 +0100

    redesign Logo

--- src/app/Logo.js
+++ src/app/Logo.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { Flex, Text, Link as RebassLink } from "rebass";
+import { Link } from "@reach/router";
 import styles from "./Logo.module.css";
 
 const Logo = () => {
   return (
-    <div className={styles.Logo}>
-      <div className={styles.Dot1}></div>
-      <div className={styles.Dot2}></div>
-    </div>
+    <RebassLink
+      as={Link}
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        textDecoration: "none",
+        width: "120px"
+      }}
+      to="/"
+    >
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        position="relative"
+        paddingX={5}
+      >
+        <div className={styles.Dot1}></div>
+        <div className={styles.Dot2}></div>
+      </Flex>
+      <Text color="body" fontWeight={600} fontSize={4}>
+        topical
+      </Text>
+    </RebassLink>
   );
 };
 
